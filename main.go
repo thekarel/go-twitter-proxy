0c34ff43a53277896bc5076500d0865af54cfe13
Karoly Szilagyi <k@szil.me>
2015-04-15 15:51:59 +0200

    feat(): API credentials are read from CLI flags
    Example runner:
    
    ```
    go run main.go \
    -ck=GlleoqPe8ArHs8YRzfD6a0naT \
    -cs=f3vX31yAl6UbLorbo0HkpOko1ncxymtCW9osxQXLUnzoGRmXZJ \
    -at=711031830-JC2aTpekaICBCPo4r6Xmm6qUw9C6bo3pp8GlK9t9 \
    -as=jUdilHooRKnYMeLPI9abjCZSBLmmQpCEMQRy9HRVfHyq6
    ```

--- main.go
+++ main.go
@@ -1,34 +1,63 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/ChimeraCoder/anaconda"
+	"log"
 	"net/http"
+	"net/url"
+)
+
+var (
+	consumerKey       = flag.String("ck", "", "Consumer key")
+	consumerSecret    = flag.String("cs", "", "Consumer secret")
+	accessToken       = flag.String("at", "", "Access token")
+	accessTokenSecret = flag.String("as", "", "Access token secret")
 )
 
 func main() {
-	handler := http.HandlerFunc(handler)
-
-	http.Handle("/", handler)
-
-	http.ListenAndServe(":8080", handler)
+	flag.Parse()
+	http.Handle("/GetUserTimeline", newServer())
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		get(w, r)
-	case "POST":
-		post(w, r)
-	default:
-		http.Error(w, "Bad request", http.StatusBadRequest)
+type server struct {
+	api *anaconda.TwitterApi
+}
+
+func newServer() *server {
+	if *consumerKey == "" || *consumerSecret == "" || *accessToken == "" || *accessTokenSecret == "" {
+		log.Fatal("You need to pass consumerKey, consumerSecret, accessToken, accessTokenSecret but some are missing. Try --help.")
+	}
+
+	anaconda.SetConsumerKey(*consumerKey)
+	anaconda.SetConsumerSecret(*consumerSecret)
+	api := anaconda.NewTwitterApi(*accessToken, *accessTokenSecret)
+
+	return &server{api: api}
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	screen_name := query.Get("screen_name")
+
+	if screen_name == "" {
+		http.Error(w, "screen_name not set, use /GetUserTimeline?screen_name=...", http.StatusBadRequest)
 		return
 	}
-}
 
-func get(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "The result is %v", "888")
-}
+	v := url.Values{}
+	v.Set("screen_name", screen_name)
 
-func post(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "SAVED result: %v", "888")
+	tweets, err := s.api.GetUserTimeline(v)
+	if err != nil {
+		log.Print(err)
+		http.Error(w, "GetUserTimeline failed", http.StatusInternalServerError)
+		return
+	}
+
+	for _, tweet := range tweets {
+		fmt.Fprintf(w, "\n\n<div>%s</div>", tweet.Text)
+	}
 }
